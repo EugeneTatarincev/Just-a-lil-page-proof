b3f0393a08d67b01cd13e7152570ce9bf45017ea
Eugene <tatr12345@bk.ru>
2020-12-27 14:49:36 +0300

    Done

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ let gulp = require('gulp');
 let sass = require('gulp-sass');
  
 gulp.task('sass', (done) => {
-  gulp.src('./sass/**/*.scss')
+  gulp.src('./sass/**/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
     done();
 });
  
 gulp.task('sass:watch', () => {
-  gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('./sass/**/style.scss', gulp.series('sass'));
 });
